165192bff26bec8f3bc1cbc3452362e2026e0924
assassin6568 <assassin65@gmail.com>
2013-04-05 12:37:59 -0400

    trying to understand git diff

--- hw.c
+++ hw.c
@@ -4,11 +4,11 @@
 
 main()
 {
-    printf("\n\nHello World\n\n");
     printf("\n\nHello World second line\n\n");
     printf("\n\nHello World third line\n\n");
     printf("\n\nHello World fourth line\n\n");
     printf("\n\nHello World fifth line\n\n");
+    printf("\n\nHello World sixth line\n\n");
 
 
 }
